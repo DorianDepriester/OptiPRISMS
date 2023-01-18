6546a2af1ef6876c2bfc26dc3e600fffdc288001
Dorian Depriester <dorian@crans.org>
2023-01-18 16:46:48 +0100

    Mode Config.ini and Very_minimal_example.py to example folder

--- example/Very_minimal_example.py
+++ example/Very_minimal_example.py
@@ -1,6 +1,6 @@
-from OptiPRISMS import runOptim
+from OptiPRISMS import optimize
 
 if __name__ == "__main__":
     # That's all. Everything you need to parametrize is actually in Config.ini
-    res = runOptim(config_file='Config.ini')
+    res = optimize(config_file='Config.ini')
     print(res)
