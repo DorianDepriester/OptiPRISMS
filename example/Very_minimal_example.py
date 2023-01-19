0a122e3c0d28b95d651eafcdc85ab5ded729fa12
Dorian Depriester <dorian@crans.org>
2023-01-19 14:59:54 +0100

    Use Config_minimal.ini

--- example/Very_minimal_example.py
+++ example/Very_minimal_example.py
@@ -1,6 +1,6 @@
 from OptiPRISMS import optimize
 
 if __name__ == "__main__":
-    # That's all. Everything you need to parametrize is actually in Config.ini
-    res = optimize(config_file='Config.ini')
+    # That's all. Everything you need to parametrize is actually in Config_minimal.ini
+    res = optimize(config_file='Config_minimal.ini')
     print(res)
