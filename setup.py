ac873d01431fb5d6a543dc4a38c910cfbc3545aa
Dorian Depriester <dorian@crans.org>
2023-02-24 09:47:45 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='OptiPRISMS',
-    version='1.0.0',
+    version='1.0.1',
     packages=[''],
     package_dir={'': 'src'},
     url='https://github.com/DorianDepriester/OptiPRISMS',
