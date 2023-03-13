f18b156ee04746fec1621ffbe6fb1c6a02ae0b5a
Dorian Depriester <dorian@crans.org>
2023-03-13 16:49:59 +0100

    New (intermediate) function matrix_projection, providing the overall matrix of basis functions triangular_projection() now takes advantages of the matrix_projection() function, and no longer uses einsum function (simply use a dot product).

--- src/triangulate.py
+++ src/triangulate.py
@@ -1,6 +1,59 @@
 import numpy as np
 from scipy.spatial import Delaunay
 
+def matrix_projection(nodes, pts):
+    """
+    Compute the projection matrix from Delaunay triangulation.
+
+    Parameters
+    ----------
+    nodes : numpy.ndarray
+        m x 2 table of node coordinates
+    pts : numpy.ndarray
+        p x 2 table of coordinates where one wants to evaluate the field
+
+    Returns
+    -------
+    mat : numpy.array
+        p x m matrix of projection coefficients. If a requested point is not in a triangle, the corresponding row will
+        be NaN.
+    inside_mesh : numpy.array
+        Array of bools indicating whether the requested points are in the mesh
+    """
+    # Perform Delaunay triangulation and find in which triangles the pts belong to
+    tri = Delaunay(nodes)
+    triangle_ids = tri.find_simplex(pts)
+    triangles = tri.simplices[triangle_ids]
+    inside_mesh = triangle_ids != -1
+
+    # Only work with points inside the mesh
+    pts_in = pts[inside_mesh]
+    toi = triangles[inside_mesh, :]  # Triangles of Interest
+
+    # Compute local coordinates of pts, wrt. their parent triangles
+    # Adapted from https://stackoverflow.com/a/57901916/12056867
+    origin = nodes[toi[:, 0], :]
+    v1 = nodes[toi[:, 1], :] - origin
+    v2 = nodes[toi[:, 2], :] - origin
+    v1r = v1.T[:, np.newaxis, :]
+    v2r = v2.T[:, np.newaxis, :]
+    mat = np.concatenate((v1r, v2r), axis=1)
+    inv_mat = np.linalg.inv(mat.T).T
+    newp = np.einsum('ijk,jk->ki', inv_mat, (pts_in - origin).T)
+
+    # Local matrices of basis functions
+    phi1 = 1 - np.sum(newp, axis=1, keepdims=True)
+    basis_funcs = np.concatenate((phi1, newp), axis=1)
+
+    # Assemble the global matrix
+    row_indices = np.repeat(np.arange(toi.shape[0]), toi.shape[1])
+    mat_in=np.zeros(shape=(len(toi), len(nodes)))
+    mat_in[row_indices,toi.flatten()]=basis_funcs.flatten()
+    mat = np.ones(shape=(len(pts), len(nodes)))*np.nan
+    mat[inside_mesh, :] = mat_in
+
+    return mat, inside_mesh
+
 
 def triangular_projection(nodes, vect_field, pts):
     """
@@ -23,39 +76,5 @@ def triangular_projection(nodes, vect_field, pts):
     inside_mesh : numpy.ndarray
         Array of bools indicating whether the requested points are in the mesh
     """
-    # Perform Delaunay triangulation and find in which triangles the pts belong to
-    tri = Delaunay(nodes)
-    triangle_ids = tri.find_simplex(pts)
-    triangles = tri.simplices[triangle_ids]
-    inside_mesh = triangle_ids != -1
-
-    # Only work with points inside the mesh
-    pts = pts[inside_mesh]
-    toi = triangles[inside_mesh, :]  # Triangles of Interest
-
-    # Compute local coordinates of pts, wrt. their parent triangles
-    # Adapted from https://stackoverflow.com/a/57901916/12056867
-    origin = nodes[toi[:, 0], :]
-    v1 = nodes[toi[:, 1], :] - origin
-    v2 = nodes[toi[:, 2], :] - origin
-    v1r = v1.T[:, np.newaxis, :]
-    v2r = v2.T[:, np.newaxis, :]
-    mat = np.concatenate((v1r, v2r), axis=1)
-    inv_mat = np.linalg.inv(mat.T).T
-    newp = np.einsum('ijk,jk->ki', inv_mat, (pts - origin).T)
-
-    # Matrices of basis functions
-    phi1 = 1 - np.sum(newp, axis=1, keepdims=True)
-    basis_funcs = np.concatenate((phi1, newp), axis=1)
-
-    if len(vect_field.shape) == 1:
-        # Force field to be at least a column vector
-        vect_field = vect_field[:, np.newaxis]
-
-    # The displacement will be nan if the requested point is outside the mesh
-    u_tri = np.ones((len(pts), vect_field.shape[1])) * np.nan
-
-    # Project node displacements with the aid of the matrix of basis function
-    u_tri[inside_mesh] = np.einsum('ik,ikj->ij', basis_funcs, vect_field[toi, :])
-
-    return u_tri, inside_mesh
+    mat, inside_mesh = matrix_projection(nodes, pts)
+    return mat.dot(vect_field), inside_mesh
