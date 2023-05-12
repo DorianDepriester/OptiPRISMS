b5e06d6aad2d3402744153090315d117d4840d89
Dorian Depriester <dorian@crans.org>
2023-05-12 12:14:02 +0200

    Update version number to 1.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='OptiPRISMS',
-    version='1.0.5',
+    version='1.1.0',
     packages=[''],
     package_dir={'': 'src'},
     url='https://github.com/DorianDepriester/OptiPRISMS',
