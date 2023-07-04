148cb442a5d480149730ded528f6a757cb3287bc
Dorian Depriester <dorian@crans.org>
2023-07-04 09:44:35 +0200

    Update setup.py
    Update version to 1.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='OptiPRISMS',
-    version='1.1.0',
+    version='1.1.1',
     packages=[''],
     package_dir={'': 'src'},
     url='https://github.com/DorianDepriester/OptiPRISMS',
